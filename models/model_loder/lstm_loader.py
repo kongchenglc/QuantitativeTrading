4547043312761f1f585b597dc6bcba55a2aedc0d
kongchenglc <kongchenglc@gmail.com>
2025-03-19 18:00:09 -0400

    feat: set transaction_fee 0

--- models/model_loder/lstm_loader.py
+++ models/model_loder/lstm_loader.py
@@ -7,13 +7,14 @@ from models.close_price_predictor import StockPricePredictor
 torch.manual_seed(42)
 np.random.seed(42)
 
-# data = get_cleaned_data()
-data = pd.read_csv("data/cleaned_data.csv", index_col="Date")
+data = get_cleaned_data()
+# data = pd.read_csv("data/cleaned_data.csv", index_col="Date")
 # data = pd.read_csv("data/test_data.csv", index_col="Date")
 
 # Load the checkpoint (saved model)
 pth_file_list = [
-    "best_model_20250221_204147.pth",  # Training data end at 2025-02-20, transaction_fee=0.01, fixed position_size=1
+    # "best_model_20250319_145441.pth",  # Training data end at 2025-02-20, transaction_fee=0.01, fixed position_size=2000
+    # "best_model_20250221_204147.pth",  # Training data end at 2025-02-20, transaction_fee=0.01, fixed position_size=1
     "best_model_20250221_151802.pth",  # Training data end at 2025-02-20, transaction_fee=0.0, position_size=1
 ]
 checkpoint = torch.load(f"./models/best_model/{pth_file_list[0]}")  # 0 is newest
@@ -27,7 +28,7 @@ print(hyperparameters)
 # Initialize the StockPricePredictor with the necessary hyperparameters
 predictor = StockPricePredictor(
     data,
-    # transaction_fee=0.0,
+    transaction_fee=0.0,
     **hyperparameters,  # Use the features saved in the checkpoint
 )
 
