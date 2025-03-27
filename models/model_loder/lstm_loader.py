03bd94cdc4a5620fc9cb4886c40c3da9f200060c
kongchenglc <kongchenglc@gmail.com>
2025-03-27 18:04:19 -0400

    feat: remove useless model

--- models/model_loder/lstm_loader.py
+++ models/model_loder/lstm_loader.py
@@ -14,11 +14,6 @@ data = get_cleaned_data()
 # Load the checkpoint (saved model)
 pth_file_list = [
     "best_model_20250326_212038.pth",  # Training data end at 2024, transaction_fee=0.00, fixed position_size=kelly
-    "best_model_20250320_100354.pth",  # Training data end at 2024, transaction_fee=0.00, fixed position_size=kelly
-    # "best_model_20250319_202207.pth",  # Training data end at 2024, transaction_fee=0.00, fixed position_size=2000
-    # "best_model_20250319_145441.pth",  # Training data end at 2024, transaction_fee=0.01, fixed position_size=2000
-    # "best_model_20250221_204147.pth",  # Training data end at 2025-02-20, transaction_fee=0.01, fixed position_size=all
-    # "best_model_20250221_151802.pth",  # Training data end at 2025-02-20, transaction_fee=0.0, position_size=all
 ]
 checkpoint = torch.load(f"./models/best_model/{pth_file_list[0]}")  # 0 is newest
 
